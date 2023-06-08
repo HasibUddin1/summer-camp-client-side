2e76c793c5a358f3237888541eef4efc5cb5c0fa
IconicLuck <uhasib959@gmail.com>
2023-06-08 20:04:04 +0600

    created a component for popular instructors

---
+++ src/Pages/Home/PopularInstructors/PopularInstructors.jsx
@@ -0,0 +1,11 @@
+
+
+const PopularInstructors = () => {
+    return (
+        <div className="mt-20">
+            <h1 className="text-4xl text-center font-bold mb-10 text-slate-800">Popular Instructors</h1>
+        </div>
+    );
+};
+
+export default PopularInstructors;

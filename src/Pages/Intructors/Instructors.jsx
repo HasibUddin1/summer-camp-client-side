e9096cfdb912542cbd70e7c2156053c023a2a4d6
IconicLuck <uhasib959@gmail.com>
2023-06-09 03:40:57 +0600

    added all instructors on instructors route

--- src/Pages/Intructors/Instructors.jsx
+++ src/Pages/Intructors/Instructors.jsx
@@ -1,9 +1,54 @@
+import { useEffect, useState } from "react";
 
 
 const Instructors = () => {
+
+    const [instructors, setInstructors] = useState([])
+
+    useEffect(() => {
+        fetch('http://localhost:5000/instructors')
+            .then(res => res.json())
+            .then(data => setInstructors(data))
+    }, [])
+
     return (
         <div>
-            <h1 className="text-5xl text-center">This is Instructors component</h1>
+            <h1 className="text-4xl text-center font-bold mt-10">All Instructors</h1>
+            <div className="w-9/12 mx-auto bg-slate-800 rounded-xl mt-10 text-white">
+                <div className="overflow-x-auto">
+                    <table className="table">
+                        {/* head */}
+                        <thead>
+                            <tr>
+                                <th className="text-white font-bold text-xl">Image</th>
+                                <th className="text-white font-bold text-xl">Name</th>
+                                <th className="text-white font-bold text-xl">Email</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                instructors.map(singleInstructor => <tr
+                                    key={singleInstructor._id}
+                                >
+                                    <td>
+                                        <div className="flex items-center space-x-3">
+                                            <div className="avatar">
+                                                <div className="mask mask-squircle w-12 h-12">
+                                                    <img src={singleInstructor.image} alt="Avatar Tailwind CSS Component" />
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        {singleInstructor.name}
+                                    </td>
+                                    <td>{singleInstructor.email}</td>
+                                </tr>)
+                            }
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
     );
 };

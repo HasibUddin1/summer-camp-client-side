c5e25c5829513664d6e07cdd3333c5be9233b846
IconicLuck <uhasib959@gmail.com>
2023-06-08 07:55:03 +0600

    loaded popular classes data from the server

--- src/Pages/Home/PopularClasses/PopularClasses.jsx
+++ src/Pages/Home/PopularClasses/PopularClasses.jsx
@@ -1,9 +1,19 @@
+import { useEffect, useState } from "react";
 
 
 const PopularClasses = () => {
+
+    const [classes, setClasses] = useState([])
+
+    useEffect(() => {
+        fetch('http://localhost:5000/popularClasses')
+        .then(res => res.json())
+        .then(data => setClasses(data))
+    },[])
+
     return (
         <div>
-            <h1 className="text-4xl text-center font-bold">Popular Classes</h1>
+            <h1 className="text-4xl text-center font-bold">Popular Classes: {classes.length}</h1>
             
         </div>
     );

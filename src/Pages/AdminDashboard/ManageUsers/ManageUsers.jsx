09e08b31d4c2dd6bc88d443e0661410fbdab7bd2
IconicLuck <uhasib959@gmail.com>
2023-06-11 16:10:52 +0600

    implemented making a user admin or instructor as an admin

--- src/Pages/AdminDashboard/ManageUsers/ManageUsers.jsx
+++ src/Pages/AdminDashboard/ManageUsers/ManageUsers.jsx
@@ -1,5 +1,8 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import { useQuery } from "@tanstack/react-query";
+// import useAdmin from "../../../hooks/useAdmin";
+// import useInstructor from "../../../hooks/useInstructor";
 
 
 
@@ -7,14 +10,43 @@ const ManageUsers = () => {
 
     const [axiosSecure] = useAxiosSecure()
 
-    const [users, setUsers] = useState([])
+    // const [users, setUsers] = useState([])
 
-    useEffect(() => {
-        axiosSecure.get('http://localhost:5000/users')
-            .then(res => {
-                setUsers(res.data)
-            })
-    }, [axiosSecure])
+    const {data: users = [], refetch} = useQuery({
+        queryKey: ['users'],
+        queryFn: async () => {
+            const res = await axiosSecure.get('/users')
+            return res.data
+        }
+    })
+
+    // const [isAdmin] = useAdmin()
+    // const [isInstructor] = useInstructor()
+    
+
+    const handleMakeInstructor = (id) => {
+        console.log('button clicked', id)
+        axiosSecure.patch(`/users/instructor/${id}`)
+        .then(res => {
+            // console.log(res.data)
+            if(res.data.modifiedCount > 0){
+                // use refetch
+                refetch()
+            }
+        })
+    }
+
+    const handleMakeAdmin = (id) => {
+        console.log('button clicked', id)
+        axiosSecure.patch(`/users/admin/${id}`)
+        .then(res => {
+            // console.log(res.data)
+            if(res.data.modifiedCount > 0){
+                // use refetch
+                refetch()
+            }
+        })
+    }
 
     return (
         <div className="w-full">
@@ -41,7 +73,7 @@ const ManageUsers = () => {
                                         <div className="flex items-center space-x-3">
                                             <div className="avatar">
                                                 <div className="mask mask-squircle w-12 h-12">
-                                                    <img src={user.image} alt="Avatar Tailwind CSS Component" />
+                                                    <img src={user?.image || user?.photo} alt="Avatar Tailwind CSS Component" />
                                                 </div>
                                             </div>
                                         </div>
@@ -50,8 +82,16 @@ const ManageUsers = () => {
                                         {user.name}
                                     </td>
                                     <td>{user.email}</td>
-                                    <td className="text-center"><button className="text-start px-6 bg-slate-300 text-black font-semibold py-2 rounded-xl hover:bg-slate-500 ease-in-out duration-200">Make Instructor</button></td>
-                                    <td className="text-center"><button className="text-start px-6 bg-slate-300 text-black font-semibold py-2 rounded-xl hover:bg-slate-500 ease-in-out duration-200">Make Admin</button></td>
+                                    <td className="text-center"><button disabled={user.role === 'instructor'} onClick={() => handleMakeInstructor(user._id)} className={
+                                        user.role === 'instructor' ?
+                                        'text-start px-6 bg-slate-300 text-slate-500 font-semibold py-2 rounded-xl' :
+                                        'text-start px-6 bg-slate-300 text-black font-semibold py-2 rounded-xl hover:bg-slate-500 ease-in-out duration-200'
+                                    }>Make Instructor</button></td>
+                                    <td className="text-center"><button disabled={user.role === 'admin'} onClick={() => handleMakeAdmin(user._id)} className={
+                                        user.role === 'admin' ?
+                                        'text-start px-6 bg-slate-300 text-slate-500 font-semibold py-2 rounded-xl' :
+                                        'text-start px-6 bg-slate-300 text-black font-semibold py-2 rounded-xl hover:bg-slate-500 ease-in-out duration-200'
+                                    }>Make Admin</button></td>
                                 </tr>)
                             }
                         </tbody>

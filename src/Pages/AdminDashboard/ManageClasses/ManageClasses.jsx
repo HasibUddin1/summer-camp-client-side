d83cf871a8d0a90e5060a44283d4eeb0b5218924
IconicLuck <uhasib959@gmail.com>
2023-08-27 19:46:22 +0600

    added a custom message for not having any classes

--- src/Pages/AdminDashboard/ManageClasses/ManageClasses.jsx
+++ src/Pages/AdminDashboard/ManageClasses/ManageClasses.jsx
@@ -56,54 +56,60 @@ const ManageClasses = () => {
 
     return (
         <div className="w-full">
-            <h1 className="text-4xl text-center font-bold mt-10">Your Selected Classes</h1>
-            <div className="w-9/12 mx-auto bg-slate-800 rounded-xl mt-10 text-white">
-                <div className="overflow-x-auto">
-                    <table className="table">
-                        {/* head */}
-                        <thead>
-                            <tr>
-                                <th className="text-white font-bold text-xl">Image</th>
-                                <th className="text-white font-bold text-xl">Name</th>
-                                <th className="text-white font-bold text-xl text-center">Action</th>
-                                <th className="text-white font-bold text-xl text-center">Action</th>
-                                <th className="text-white font-bold text-xl text-center">Action</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {
-                                classes.map(singleClass => <tr
-                                    key={singleClass._id}
-                                >
-                                    <td>
-                                        <div className="flex items-center space-x-3">
-                                            <div className="avatar">
-                                                <div className="mask mask-squircle w-12 h-12">
-                                                    <img src={singleClass.image} alt="Avatar Tailwind CSS Component" />
+            {
+                classes.length === 0 ?
+                <h1 className="text-4xl text-center font-bold">Your website does not have any classes</h1> :
+                <div className="w-full">
+                    <h1 className="text-4xl text-center font-bold mt-10">Your Selected Classes</h1>
+                    <div className="w-9/12 mx-auto bg-slate-800 rounded-xl mt-10 text-white">
+                        <div className="overflow-x-auto">
+                            <table className="table">
+                                {/* head */}
+                                <thead>
+                                    <tr>
+                                        <th className="text-white font-bold text-xl">Image</th>
+                                        <th className="text-white font-bold text-xl">Name</th>
+                                        <th className="text-white font-bold text-xl text-center">Action</th>
+                                        <th className="text-white font-bold text-xl text-center">Action</th>
+                                        <th className="text-white font-bold text-xl text-center">Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {
+                                        classes.map(singleClass => <tr
+                                            key={singleClass._id}
+                                        >
+                                            <td>
+                                                <div className="flex items-center space-x-3">
+                                                    <div className="avatar">
+                                                        <div className="mask mask-squircle w-12 h-12">
+                                                            <img src={singleClass.image} alt="Avatar Tailwind CSS Component" />
+                                                        </div>
+                                                    </div>
                                                 </div>
-                                            </div>
-                                        </div>
-                                    </td>
-                                    <td>
-                                        {singleClass.name}
-                                    </td>
-                                    <td className="text-center"><button onClick={() => handleApprove(singleClass._id)} disabled={singleClass.status === 'approved'} className={
-                                        singleClass.status === 'approved' ?
-                                            'text-start px-6 font-semibold py-2' :
-                                            'text-start px-6 bg-slate-300 text-black font-semibold py-2 rounded-xl hover:bg-slate-500 ease-in-out duration-200'
-                                    }>{singleClass.status === 'approved' ? 'Approved' : 'Approve'}</button></td>
-                                    <td className="text-center"><button onClick={() => handleDeny(singleClass._id)} disabled={singleClass.status === 'denied'} className={
-                                        singleClass.status === 'denied' ?
-                                            'text-start px-6 bg-slate-300 text-slate-500 font-semibold py-2 rounded-xl' :
-                                            'text-start px-6 bg-slate-300 text-black font-semibold py-2 rounded-xl hover:bg-slate-500 ease-in-out duration-200'
-                                    }>Deny</button></td>
-                                    <td className="text-center"><Link to={`/dashboard/sendFeedback/${singleClass._id}`}><button className='text-start px-6 bg-slate-300 text-black font-semibold py-2 rounded-xl hover:bg-slate-500 ease-in-out duration-200'>Send Feedback</button></Link></td>
-                                </tr>)
-                            }
-                        </tbody>
-                    </table>
+                                            </td>
+                                            <td>
+                                                {singleClass.name}
+                                            </td>
+                                            <td className="text-center"><button onClick={() => handleApprove(singleClass._id)} disabled={singleClass.status === 'approved'} className={
+                                                singleClass.status === 'approved' ?
+                                                    'text-start px-6 font-semibold py-2' :
+                                                    'text-start px-6 bg-slate-300 text-black font-semibold py-2 rounded-xl hover:bg-slate-500 ease-in-out duration-200'
+                                            }>{singleClass.status === 'approved' ? 'Approved' : 'Approve'}</button></td>
+                                            <td className="text-center"><button onClick={() => handleDeny(singleClass._id)} disabled={singleClass.status === 'denied'} className={
+                                                singleClass.status === 'denied' ?
+                                                    'text-start px-6 bg-slate-300 text-slate-500 font-semibold py-2 rounded-xl' :
+                                                    'text-start px-6 bg-slate-300 text-black font-semibold py-2 rounded-xl hover:bg-slate-500 ease-in-out duration-200'
+                                            }>Deny</button></td>
+                                            <td className="text-center"><Link to={`/dashboard/sendFeedback/${singleClass._id}`}><button className='text-start px-6 bg-slate-300 text-black font-semibold py-2 rounded-xl hover:bg-slate-500 ease-in-out duration-200'>Send Feedback</button></Link></td>
+                                        </tr>)
+                                    }
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
                 </div>
-            </div>
+            }
         </div>
     );
 };
--- src/Pages/AdminDashboard/ManageUsers/ManageUsers.jsx
+++ src/Pages/AdminDashboard/ManageUsers/ManageUsers.jsx
@@ -15,7 +15,7 @@ const ManageUsers = () => {
 
     // const [users, setUsers] = useState([])
 
-    const {data: users = [], refetch} = useQuery({
+    const { data: users = [], refetch } = useQuery({
         queryKey: ['users'],
         queryFn: async () => {
             const res = await axiosSecure.get('/users')
@@ -25,82 +25,88 @@ const ManageUsers = () => {
 
     // const [isAdmin] = useAdmin()
     // const [isInstructor] = useInstructor()
-    
+
 
     const handleMakeInstructor = (id) => {
         // console.log('button clicked', id)
         axiosSecure.patch(`/users/instructor/${id}`)
-        .then(res => {
-            // console.log(res.data)
-            if(res.data.modifiedCount > 0){
-                // use refetch
-                refetch()
-            }
-        })
+            .then(res => {
+                // console.log(res.data)
+                if (res.data.modifiedCount > 0) {
+                    // use refetch
+                    refetch()
+                }
+            })
     }
 
     const handleMakeAdmin = (id) => {
         // console.log('button clicked', id)
         axiosSecure.patch(`/users/admin/${id}`)
-        .then(res => {
-            // console.log(res.data)
-            if(res.data.modifiedCount > 0){
-                // use refetch
-                refetch()
-            }
-        })
+            .then(res => {
+                // console.log(res.data)
+                if (res.data.modifiedCount > 0) {
+                    // use refetch
+                    refetch()
+                }
+            })
     }
 
     return (
         <div className="w-full">
-            <h1 className="text-4xl text-center font-bold mt-10">All Users</h1>
-            <div className="w-9/12 mx-auto bg-slate-800 rounded-xl mt-10 text-white">
-                <div className="overflow-x-auto">
-                    <table className="table">
-                        {/* head */}
-                        <thead>
-                            <tr>
-                                <th className="text-white font-bold text-xl">Image</th>
-                                <th className="text-white font-bold text-xl">Name</th>
-                                <th className="text-white font-bold text-xl">Email</th>
-                                <th className="text-white font-bold text-xl text-center">Action</th>
-                                <th className="text-white font-bold text-xl text-center">Action</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {
-                                users.map(user => <tr
-                                    key={user._id}
-                                >
-                                    <td>
-                                        <div className="flex items-center space-x-3">
-                                            <div className="avatar">
-                                                <div className="mask mask-squircle w-12 h-12">
-                                                    <img src={user?.image || user?.photo} alt="Avatar Tailwind CSS Component" />
+            {
+                users.length === 0 ? 
+                <h1 className="text-4xl text-center font-bold">Your website does not have any users</h1> :
+                <div className="w-full">
+                    <h1 className="text-4xl text-center font-bold mt-10">All Users</h1>
+                    <div className="w-9/12 mx-auto bg-slate-800 rounded-xl mt-10 text-white">
+                        <div className="overflow-x-auto">
+                            <table className="table">
+                                {/* head */}
+                                <thead>
+                                    <tr>
+                                        <th className="text-white font-bold text-xl">Image</th>
+                                        <th className="text-white font-bold text-xl">Name</th>
+                                        <th className="text-white font-bold text-xl">Email</th>
+                                        <th className="text-white font-bold text-xl text-center">Action</th>
+                                        <th className="text-white font-bold text-xl text-center">Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {
+                                        users.map(user => <tr
+                                            key={user._id}
+                                        >
+                                            <td>
+                                                <div className="flex items-center space-x-3">
+                                                    <div className="avatar">
+                                                        <div className="mask mask-squircle w-12 h-12">
+                                                            <img src={user?.image || user?.photo} alt="Avatar Tailwind CSS Component" />
+                                                        </div>
+                                                    </div>
                                                 </div>
-                                            </div>
-                                        </div>
-                                    </td>
-                                    <td>
-                                        {user.name}
-                                    </td>
-                                    <td>{user.email}</td>
-                                    <td className="text-center"><button disabled={user.role === 'instructor'} onClick={() => handleMakeInstructor(user._id)} className={
-                                        user.role === 'instructor' ?
-                                        'text-start px-6 bg-slate-300 text-slate-500 font-semibold py-2 rounded-xl' :
-                                        'text-start px-6 bg-slate-300 text-black font-semibold py-2 rounded-xl hover:bg-slate-500 ease-in-out duration-200'
-                                    }>Make Instructor</button></td>
-                                    <td className="text-center"><button disabled={user.role === 'admin'} onClick={() => handleMakeAdmin(user._id)} className={
-                                        user.role === 'admin' ?
-                                        'text-start px-6 bg-slate-300 text-slate-500 font-semibold py-2 rounded-xl' :
-                                        'text-start px-6 bg-slate-300 text-black font-semibold py-2 rounded-xl hover:bg-slate-500 ease-in-out duration-200'
-                                    }>Make Admin</button></td>
-                                </tr>)
-                            }
-                        </tbody>
-                    </table>
+                                            </td>
+                                            <td>
+                                                {user.name}
+                                            </td>
+                                            <td>{user.email}</td>
+                                            <td className="text-center"><button disabled={user.role === 'instructor'} onClick={() => handleMakeInstructor(user._id)} className={
+                                                user.role === 'instructor' ?
+                                                    'text-start px-6 bg-slate-300 text-slate-500 font-semibold py-2 rounded-xl' :
+                                                    'text-start px-6 bg-slate-300 text-black font-semibold py-2 rounded-xl hover:bg-slate-500 ease-in-out duration-200'
+                                            }>Make Instructor</button></td>
+                                            <td className="text-center"><button disabled={user.role === 'admin'} onClick={() => handleMakeAdmin(user._id)} className={
+                                                user.role === 'admin' ?
+                                                    'text-start px-6 bg-slate-300 text-slate-500 font-semibold py-2 rounded-xl' :
+                                                    'text-start px-6 bg-slate-300 text-black font-semibold py-2 rounded-xl hover:bg-slate-500 ease-in-out duration-200'
+                                            }>Make Admin</button></td>
+                                        </tr>)
+                                    }
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
                 </div>
-            </div>
+            }
         </div>
     );
 };
